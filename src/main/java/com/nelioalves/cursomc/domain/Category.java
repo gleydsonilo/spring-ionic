4c1eda5f5179aabe143a7a850de25f57e0fe41f3
Gleydsonilo Negre <gleydsontaichou@gmail.com>
2021-08-08 12:16:02 -0300

    Include H2 database

--- src/main/java/com/nelioalves/cursomc/domain/Category.java
+++ src/main/java/com/nelioalves/cursomc/domain/Category.java
@@ -2,6 +2,10 @@ package com.nelioalves.cursomc.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -9,9 +13,12 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @EqualsAndHashCode.Exclude
