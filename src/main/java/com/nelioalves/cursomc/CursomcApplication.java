c7f1a020d4a6c5cf87624b0c146cb419ae6683e3
Gleydsonilo Negre <gleydsontaichou@gmail.com>
2021-08-08 14:37:16 -0300

    Creating instantiation operation

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,13 +1,29 @@
 package com.nelioalves.cursomc;
 
+import com.nelioalves.cursomc.domain.Category;
+import com.nelioalves.cursomc.repositories.CategoryRepositoy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepositoy categoryRepositoy;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepositoy.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
